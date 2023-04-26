1343a3e8e7c64bb61bacea38ca43486d48938685
Eluwing <abek0403@gmail.com>
2023-04-26 20:31:49 +0900

    [Fix] add comment in constant file

--- src/Constants/Common.tsx
+++ src/Constants/Common.tsx
@@ -3,14 +3,16 @@ import { IGetMoviesResult, IGetPopularMoviesResult } from '../api';
 export const API_KEY = '9d9e113697aea324490c0ee7b9da45dd';
 export const BASE_PATH = 'https://api.themoviedb.org/3';
 
+// Add Me : if create new Silder , Add key name
 export const enum SCREEN_TYPES {
   NOW_PLAYING_MOVIE = 'now_playing_movie',
   POPULAR_MOVIE = 'popular_movie',
 }
 
-// Add Me : if create interface type of API
+// Add Me : if create new interface type of API, Add interface name
 export type API_INTERFACE_TYPES = IGetMoviesResult | IGetPopularMoviesResult;
 
+// Add Me : if create new useQuery Key type, Add key name
 export const enum SCREEN_QUERY_KEY {
   MOVIE = 'movie',
   TV = 'tv',
