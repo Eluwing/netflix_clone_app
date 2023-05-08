9480afd14b5e5472abd07f3306f9e50612aab083
Eluwing <abek0403@gmail.com>
2023-05-08 19:35:42 +0900

    [Feature]Add Banner component in tv page component

--- src/Routes/Tv.tsx
+++ src/Routes/Tv.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
-import { makeImagePath } from '../utils';
 import { IGetMoviesResult } from '../api';
+import Banner from '../Components/Banner';
 
 const Wrapper = styled.div`
   background: black;
@@ -12,32 +12,6 @@ const Loader = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-`;
-
-const Banner = styled.div<{ bgPhoto: string }>`
-  height: 100%;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  padding: 60px;
-  background-image: linear-gradient(rgba(0, 0, 0, 1), rgba(0, 0, 0, 0)),
-    url(${(props) => props.bgPhoto});
-  background-size: cover;
-`;
-
-const Title = styled.h2`
-  font-size: 68px;
-  margin-bottom: 20px;
-`;
-
-const OverView = styled.div`
-  font-size: 30px;
-  width: 50%;
-`;
-
-const SliderArea = styled.div`
-  padding-bottom: 200px;
-  margin: 30px 0px;
 `;
 
 function Tv(): JSX.Element {
@@ -58,10 +32,11 @@ function Tv(): JSX.Element {
       {isLoading ? (
         <Loader>Loading...</Loader>
       ) : (
-        <Banner bgPhoto={makeImagePath(data?.results[0].backdrop_path ?? '')}>
-          <Title>{data?.results[0].title}</Title>
-          <OverView>{data?.results[0].overview}</OverView>
-        </Banner>
+        <Banner
+          backgroundImagePath={data?.results[0].backdrop_path}
+          title={data?.results[0].title}
+          overview={data?.results[0].overview}
+        />
       )}
     </Wrapper>
   );
