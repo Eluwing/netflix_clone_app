7c4a7ab96176d35690a96b009373b7b5ef30bbe2
Eluwing <abek0403@gmail.com>
2023-05-24 19:41:11 +0900

    [Feature]Slider Box Id Generator

--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,7 @@
 export function makeImagePath(id: string, format?: string): string {
   return `https://image.tmdb.org/t/p/${format !== undefined ? format : 'original'}/${id}`;
 }
+
+export function getSliderBoxId(screenNum: number, sliderAndScreenType: string): string {
+  return String(screenNum).concat(sliderAndScreenType);
+}
