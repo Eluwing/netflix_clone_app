8040f8c82141ab18bd6c37ce91dea96a7db1cd58
Eluwing <abek0403@gmail.com>
2023-08-30 19:45:39 +0900

    [Feature] Add Simple get keywrod moive data from keyword movie search API

--- src/Routes/Search.tsx
+++ src/Routes/Search.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
+import { IGetMovieKeywordSearchResult, getMoiveKeywordSearch } from '../api';
+import { useQuery } from 'react-query';
 
 function Search(): JSX.Element {
   const location = useLocation();
   const keyword = new URLSearchParams(location.search).get('keyword');
-  console.log({ keyword });
-  return <>{null}</>;
+  const { data } = useQuery<IGetMovieKeywordSearchResult>(
+    ['Search', 'Keyword'],
+    async () => await getMoiveKeywordSearch(String(keyword)),
+  );
+  return (
+    <>
+      {data?.results.map((search) => (
+        <>{search.title}</>
+      ))}
+    </>
+  );
 }
 export default Search;
