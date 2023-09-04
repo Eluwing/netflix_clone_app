43b3d45ba008f48020f68fafde0a1cc3a6e714cc
Eluwing <abek0403@gmail.com>
2023-09-04 20:05:04 +0900

    [Feature]Add Common Component with display Search Result

---
+++ src/Components/ScreenSearchResult.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function ScreenSearchResult(): JSX.Element {
+  return (
+    <>
+      <div>{'ScreenSearch Component'}</div>
+    </>
+  );
+}
+export default ScreenSearchResult;
