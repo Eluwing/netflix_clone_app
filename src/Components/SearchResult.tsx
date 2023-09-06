9f07875918eaeaee2e8605cf6de3a7da57eea0a5
Eluwing <abek0403@gmail.com>
2023-09-06 20:29:39 +0900

    [Feature] Set Initial value of Search Result Component with data obtained API

--- src/Components/SearchResult.tsx
+++ src/Components/SearchResult.tsx
@@ -1,16 +1,39 @@
-import React, { useState } from 'react';
-import { IGetMovieKeywordSearchResult, IGetTvKeywordSearchResult } from '../api';
+import React, { useEffect, useState } from 'react';
+import {
+  IGetMovieKeywordSearchResult,
+  IGetTvKeywordSearchResult,
+  getMovieKeywordSearch,
+  getTvKeywordSearch,
+} from '../api';
+import { SCREEN_TYPES } from '../Constants/Common';
 
 interface SearchResultProps {
+  keyword: string;
   screenType: number;
 }
 
-function SearchResult({ screenType }: SearchResultProps): JSX.Element {
-  const [data, setData] = useState<IGetTvKeywordSearchResult | IGetMovieKeywordSearchResult>();
+function SearchResult({ keyword, screenType }: SearchResultProps): JSX.Element {
+  const [data, setData] =
+    useState<Promise<IGetTvKeywordSearchResult | IGetMovieKeywordSearchResult>>();
 
-  // useEffect(()=>{
+  switch (screenType) {
+    case SCREEN_TYPES.TV:
+      useEffect(() => {
+        setData(async () => {
+          return await getTvKeywordSearch(keyword);
+        });
+      }, [data]);
+    // eslint-disable-next-line no-fallthrough
+    case SCREEN_TYPES.MOVIES:
+      useEffect(() => {
+        setData(async () => {
+          return await getMovieKeywordSearch(keyword);
+        });
+      }, [data]);
+    // eslint-disable-next-line no-fallthrough
+    default:
+  }
 
-  // },[data]);
   return (
     <>
       <div>{'ScreenSearch Component'}</div>
