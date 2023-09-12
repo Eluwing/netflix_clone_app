747b9745992c21ff52e78ef0d38c31a707c7dcfd
Eluwing <abek0403@gmail.com>
2023-09-12 19:32:31 +0900

    Modify set initial value logic and Add css component

--- src/Components/SearchResult.tsx
+++ src/Components/SearchResult.tsx
@@ -6,39 +6,107 @@ import {
   getTvKeywordSearch,
 } from '../api';
 import { SCREEN_TYPES } from '../Constants/Common';
+import { motion } from 'framer-motion';
+import styled from 'styled-components';
 
 interface SearchResultProps {
   keyword: string | null;
   screenType: number;
 }
 
+const Box = styled(motion.div)<{ bgphoto: string }>`
+  background-color: white;
+  background-image: url(${(props) => props.bgphoto});
+  background-size: cover;
+  background-position: center center;
+  height: 200px;
+  &:first-child {
+    transform-origin: center left;
+  }
+  &:last-child {
+    transform-origin: center right;
+  }
+`;
+
+const Row = styled(motion.div)`
+  display: grid;
+  gap: 5px;
+  grid-template-columns: repeat(6, 1fr);
+`;
+
+const BoxListArea = styled.div`
+  width: 100%;
+`;
+
+const Info = styled(motion.div)`
+  padding: 20px;
+  background-color: ${(props) => props.theme.black.lighter};
+  opacity: 0;
+  position: absolute;
+  width: -webkit-fill-available;
+  bottom: 0;
+  &:first-child {
+    padding-left: 10px;
+    padding-right: 20px;
+    padding-top: 20px;
+    padding-bottom: 20px;
+  }
+`;
+
+const InfoTitle = styled(motion.div)`
+  padding-left: 10px;
+  text-align: center;
+  font-size: 18px;
+`;
+
 function SearchResult({ keyword, screenType }: SearchResultProps): JSX.Element {
-  const [data, setData] =
-    useState<Promise<IGetTvKeywordSearchResult | IGetMovieKeywordSearchResult>>();
+  const [data, setData] = useState<IGetTvKeywordSearchResult | IGetMovieKeywordSearchResult>();
 
   switch (screenType) {
     case SCREEN_TYPES.TV:
       useEffect(() => {
-        setData(async () => {
-          return await getTvKeywordSearch(keyword);
+        void getTvKeywordSearch(keyword).then((data) => {
+          setData(data);
         });
       }, []);
       break;
     case SCREEN_TYPES.MOVIES:
       useEffect(() => {
-        setData(async () => {
-          return await getMovieKeywordSearch(keyword);
+        void getMovieKeywordSearch(keyword).then((data) => {
+          setData(data);
         });
       }, []);
       break;
     default:
-      break;
+      throw new Error('Not exist Screen Type Code');
   }
 
-  console.log({ screenType, data });
   return (
     <>
-      <div>{'ScreenSearch Component'}</div>
+      <BoxListArea>
+        <Row
+          initial="hidden"
+          animate="visible"
+          exit="exit"
+          transition={{ type: 'tween', duration: 1 }}
+        >
+          {/* {data?.results
+            .map((movie) => (
+              <Box
+                key={0}
+                onClick={() => onBoxClicked(getSliderBoxId(movie.id, sliderAndScreenType))}
+                bgphoto={makeImagePath(movie.backdrop_path ?? '', 'w500')}
+              >
+                {(movie.title && movie.title) ?? (movie.name && movie.name)}
+                <Info variants={infoVariants}>
+                  <InfoTitle>
+                    {(movie.title && movie.title) ?? (movie.name && movie.name)}
+                  </InfoTitle>
+                </Info>
+              </Box>
+            ))} */}
+        </Row>
+      </BoxListArea>
     </>
   );
 }
